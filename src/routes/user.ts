98658ec3c5468a7dfd0372afb66fea56ad8d2b46
Nick Clifford <nick@nickclifford.me>
2019-04-11 23:20:11 -0500

    next question

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express';
+import Answer from '../models/Answer';
+import Question from '../models/Question';
+import Unit from '../models/Unit';
 import User from '../models/User';
+import { pickProps } from '../utils/functions';
 import { asyncHandler, requireLoggedIn } from '../utils/middleware';
 import { success } from '../utils/responses';
 
@@ -13,6 +17,37 @@ router.get('/', requireLoggedIn, asyncHandler(async (req, res) => {
 	delete userData.confirmationHash;
 
 	success(res, userData);
+}));
+
+router.get('/next-question', requireLoggedIn, asyncHandler(async (req, res) => {
+	const user = await User.findById(req.authorized!.user, {
+		include: [{
+			model: Answer,
+			include: [Question]
+		}]
+	});
+
+	let nextQuestion: object | null = null;
+
+	const answers = user!.answers.sort((a, b) => b.submittedAt.getTime() - a.submittedAt.getTime());
+	if (answers.length !== 0) {
+		const unitId = answers[0].question.unitId;
+		const answeredQuestionIds = answers.map(a => a.question).filter(q => q.unitId === unitId).map(q => q.id);
+
+		const unit = await Unit.findById(unitId, { include: [Question] });
+		const unitQuestionIds = unit!.questions.map(q => q.id);
+
+		const questionDifference = unitQuestionIds.filter(q => !answeredQuestionIds.includes(q));
+
+		if (questionDifference.length !== 0) { // If not all the questions have been completed
+			nextQuestion = pickProps(
+				unit!.questions.find(q => q.id === questionDifference[0])!,
+				['id', 'questionType', 'unitId']
+			);
+		}
+	}
+
+	success(res, { nextQuestion });
 }));
 
 export default router;
